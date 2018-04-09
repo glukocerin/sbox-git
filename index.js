5cce797ce67bd09c9af21d97a71cbff7a1a28687
Attila Csordas <ati.csordas@gmail.com>
2018-04-09 10:55:43 +0200

    add some feature

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var a = 'Hello World'
 console.log(a)
 
+<<<<<<< HEAD
 var d = 123
 var e = 345
 
 console.log(d, e)
+=======
+var d = 'asdasd'
+
+var g = 'asd23'
+
+console.log(d, g)
+>>>>>>> add some feature
